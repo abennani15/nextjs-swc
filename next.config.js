b74604c04220bbc0b6be4c6532a876355ff53f5c
Ahmed Bennani <a.bennani@thinkweb.ma>
2022-10-06 08:28:24 +0100

    init next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+	experimental: {
+		swcPlugins: [
+			[
+				"swc-plugin-coverage-instrument",
+				{
+					coverageVariable: "__coverage__",
+					esModules: true,
+					debug: true,
+					instrumentLog: {
+						level: "info"
+					}
+				}
+			]
+		]
+	},
+	webpack: (webpackConfig) => {
+		const config = webpackConfig;
+
+		config.resolve.alias.components = path.join(__dirname, "src/adapters/primary/components");
+		config.resolve.alias.hooks = path.join(__dirname, "src/adapters/primary/hooks");
+
+		return config;
+	}
 }
 
 module.exports = nextConfig
